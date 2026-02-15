e41128f274fd3abc503db3c79f525cf9284c55f8
SAHIL CHACHANE <sahilchachane11@gmail.com>
2026-02-15 10:58:16 +0000

    only fit view more remove extra space  (_for element <LinkComponent>_)

--- src/components/sections/services.tsx
+++ src/components/sections/services.tsx
@@ -43,7 +43,7 @@ export function Services() {
                     <h3 className="text-2xl font-bold font-body mt-6 mb-4">{service.title}</h3>
                     <p className="text-sm font-body text-foreground/70">{service.description}</p>
                 </div>
-                <Button asChild size="sm" className="mt-8 h-8 px-3 text-xs bg-accent hover:bg-accent/90 text-accent-foreground rounded-full transform -skew-x-12">
+                <Button asChild className="mt-5 py-2 px-4 text-xs bg-accent hover:bg-accent/90 text-accent-foreground rounded-full transform -skew-x-12 self-center">
                     <Link href={service.href}><span className="inline-block transform skew-x-12">View More</span></Link>
                 </Button>
             </Card>
